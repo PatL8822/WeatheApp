09360e091b766ebfdd51de5d583bd0dc58487046
Isaiah411013 <Archangel_r320@hotmail.com>
2022-09-23 21:40:27 -0700

    overhaul of old code html set css set redy for information input

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,68 @@
 const cityInput = document.querySelector('#city');
 const submitBtn = document.querySelector('#submit');
 const cityRef = document.querySelector('#cityRef');
-// var APIKEY =
+var apiKey = "eea20da06cb196d467d9e8ec12c0d5bd";
 var city;
 var cityName;
+var cities = [];
 
-submitBtn.addEventListener('click', () => {
-    console.log("Inside event Listener");
-    console.log("val of cityInput", cityInput.value );
-    cityName =cityInput.value;
-    var newLI = document.createElement('li');
+submitBtn.addEventListener('click', citySearchHistory);
+
+function citySearchHistory(){
+    var newLI = document.createElement('ul');
     var btn = document.createElement('button');
-    btn.textContent = cityName;
     cityRef.appendChild(newLI);
     newLI.appendChild(btn);
-});
+    cityName =cityInput.value;
+    cities.push(cityName);
+    btn.textContent = cityName;
+    localStorage.setItem("City",JSON.stringify(cities))
+    localStorage.getItem("City");
+}
 
-// localStorage.getItem("data");
-// var cities=[city1,city2]
+function fetchWeather(){
 
-// localStorage.setItem("data",JSON.stringify(cities))
+    var weatherURLTwo = `https://api.openweathermap.org/data/2.5/weather?q=${'Puyallup'}&appid=${apiKey}`;
+
+    fetch(weatherURLTwo).then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// saveButton.addEventListener("click", function(event) {
+//     event.preventDefault();
+    
+//     var studentGrade = {
+//       student: student.value,
+//       grade: grade.value,
+//       comment: comment.value.trim()
+//     };
+    
+//     localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
+//     renderMessage();
+    
+//     });
+    
+//     function renderMessage() {
+//       var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
+//       if (lastGrade !== null) {
+//         document.querySelector(".message").textContent = lastGrade.student + 
+//         " received a/an " + lastGrade.grade
+//       }
+//     }
